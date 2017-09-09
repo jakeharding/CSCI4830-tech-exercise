a76200e4bce7fdab27243b118092b86495cb98a3
Jake Harding <jacobeharding@gmail.com>
2017-09-09 11:49:18 -0500

    Add files, views and urls.

--- tech_exercise/index/views.py
+++ tech_exercise/index/views.py
@@ -6,11 +6,33 @@ SPDX-License-Identifier: MIT
 Author(s) of this file:
   J. Harding
 
-Views for the index page
+Views for the BlogEntry pages
 """
 
-from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.base import ContextMixin
+from tech_exercise.index.models import BlogEntry
 
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class BlogContextMixin(ContextMixin):
+    page_heading = ""
+
+    def get_context_data(self, **kwargs):
+        cxt = super().get_context_data(**kwargs)
+        cxt.update({
+            "page_heading": self.page_heading
+        })
+        return cxt
+
+
+class BlogEntryListView(ListView , BlogContextMixin):
+    model = BlogEntry
+    page_heading = "Blog Entry List"
+
+
+class CreateBlogEntryView(CreateView, BlogContextMixin):
+    model = BlogEntry
+    fields = ['title', 'author', 'email', 'article']
+    page_heading = "Create Blog Entry"
+
--- tech_exercise/urls.py
+++ tech_exercise/urls.py
@@ -1,21 +1,8 @@
-"""tech_exercise URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
-from tech_exercise.index.views import IndexView
+from tech_exercise.index.views import BlogEntryListView, CreateBlogEntryView
 
 urlpatterns = [
-    url(r'^', IndexView.as_view()),
+    url(r'^$', BlogEntryListView.as_view(), name="blog-entry-list"),
+    url(r'^create/$', CreateBlogEntryView.as_view(), name="blog-entry-create"),
+    url(r'^detail/(?P<pk>\d+)/$', CreateBlogEntryView.as_view(), name="blog-entry-detail"),
 ]
