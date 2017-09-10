d3b762d85476d463e038c91e42e64a30e120c6c8
Jake Harding <jacobeharding@gmail.com>
2017-09-09 19:01:43 -0500

    Layout create page.

--- tech_exercise/index/views.py
+++ tech_exercise/index/views.py
@@ -11,8 +11,24 @@ Views for the BlogEntry pages
 
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.forms import ModelForm
+
 from django.views.generic.base import ContextMixin
 from tech_exercise.index.models import BlogEntry
+
+
+class BlogEntryForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for f in self.visible_fields():
+            f.field.widget.attrs['class'] = 'form-control mb-4'
+            f.field.widget.attrs['placeholder'] = f.label
+            f.label = ''
+
+    class Meta:
+        model = BlogEntry
+        exclude = ['created_at']
 
 
 class BlogContextMixin(ContextMixin):
@@ -21,18 +37,18 @@ class BlogContextMixin(ContextMixin):
     def get_context_data(self, **kwargs):
         cxt = super().get_context_data(**kwargs)
         cxt.update({
-            "page_heading": self.page_heading
+            "page_heading": self.page_heading,
         })
         return cxt
 
 
-class BlogEntryListView(ListView , BlogContextMixin):
+class BlogEntryListView(ListView, BlogContextMixin):
     model = BlogEntry
     page_heading = "Blog Entry List"
 
 
 class CreateBlogEntryView(CreateView, BlogContextMixin):
     model = BlogEntry
-    fields = ['title', 'author', 'email', 'article']
     page_heading = "Create Blog Entry"
+    form_class = BlogEntryForm
 
